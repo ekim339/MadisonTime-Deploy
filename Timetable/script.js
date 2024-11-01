48697d60e7ac2ac6013975ff70faf2bd5fc96829
eugene165413 <134651437+eugene165413@users.noreply.github.com>
2024-11-01 01:16:55 -0500

    second commit

--- Timetable/script.js
+++ Timetable/script.js
@@ -1,11 +1,18 @@
-const btnAdd = document.querySelector('add-course-btn');
+const btnAddCourse = document.querySelector('.add-course-btn');
 const modalAdd = document.querySelector('.modal');
-
+const modalWrapper = document.querySelector('.modal-wrapper');
+const addModalX = document.querySelector('.modal-header i');
+const btnAdd = document.querySelector('.add');
 /*************************************************/
 /*************************************************/
 
-// Default display
-btnAdd.addEventListener('click', () => {
-  modal.style.displat = 'block';
-  modal.classList.add('modal-wrapper-display');
+// Add modal
+btnAddCourse.addEventListener('click', () => {
+  modalAdd.style.display = 'block';
+  modalWrapper.classList.add('modal-wrapper-display');
 })
+
+addModalX.addEventListener('click', () => {
+  modalAdd.style.display = 'none';
+  modalWrapper.classList.remove('modal-wrapper-display');
+});
