adfbbc4718eb49910077d8198c934a78f8cb4088
eugene165413 <134651437+eugene165413@users.noreply.github.com>
2025-10-08 13:23:50 -0500

    attaching aws s3

--- backend/MadisonTime/MadisonTime/settings_production.py
+++ backend/MadisonTime/MadisonTime/settings_production.py
@@ -48,9 +48,16 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Media files
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = '/uploads/'
+# # Media files
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# MEDIA_URL = '/media/'
+
+# AWS S3 settings
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = 'us-west-1' 
 
 # Security settings
 SECURE_BROWSER_XSS_FILTER = True
